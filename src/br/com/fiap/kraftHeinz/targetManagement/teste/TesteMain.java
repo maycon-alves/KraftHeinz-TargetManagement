b2903415ae55c16782ecd04735bb16b479f56c50
maycon-alves <maycon_valdemiro@hotmail.com>
2023-05-28 20:56:19 -0300

    estabelecendo conexao com o oracle

--- src/br/com/fiap/kraftHeinz/targetManagement/teste/TesteMain.java
+++ src/br/com/fiap/kraftHeinz/targetManagement/teste/TesteMain.java
@@ -1,10 +1,12 @@
 package br.com.fiap.kraftHeinz.targetManagement.teste;
 
+import br.com.fiap.kreftHeinz.targetManagement.DAO.TesteDAO;
+
 public class TesteMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		TesteDAO testeDAO = new TesteDAO();
 	}
 
 }
